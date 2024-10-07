c36f771b1bdc122750fc627a7001cf640f3d154e
Lê Đức Đồng <v.dongld8@vinai.io>
2024-10-07 03:01:45 +0000

    fix

--- script/train_sft_keallm_8.sh
+++ script/train_sft_keallm_8.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2,3 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --master_addr 127.0.0.1 --master_port 23956\
+CUDA_VISIBLE_DEVICES=0 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --master_addr 127.0.0.1 --master_port 23956\
                     src/train.py --output_dir ./save/sft/metaqa/keallm/keallm_8 \
                     --stage sft \
                     --hop 1-hop \
