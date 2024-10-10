75a6858e95c4ad37ac6f9eaf1daca2b4b75fa5db
Lê Đức Đồng <v.dongld8@vinai.io>
2024-10-10 08:31:46 +0000

    fix

--- script/train_sft_keallm_32.sh
+++ script/train_sft_keallm_32.sh
@@ -2,13 +2,13 @@ CUDA_VISIBLE_DEVICES=0 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --m
                     src/train.py --output_dir ./save/sft/metaqa/keallm/keallm_32 \
                     --stage sft \
                     --hop 1-hop \
-                    --model_name_or_path meta-llama/Llama-2-7b-chat-hf \
+                    --model_name_or_path save/sft/metaqa/keallm/keallm_32  \
                     --language_model_path meta-llama/Llama-2-7b-chat-hf \
                     --kge_model_path ledong0110/MetaQA-Roberta-Base \
                     --model_type keallm \
                     --template llama2_keallm \
                     --num_query_tokens 32 \
-                    --train_from_scratch true \
+                    --train_from_scratch false \
                     --num_train_epochs 3 \
                     --save_total_limit 3 \
                     --load_best_model_at_end true\
