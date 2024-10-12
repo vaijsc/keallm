f47147286d8fcf8d87254d3f26a2170160f530f6
Lê Đức Đồng <v.dongld8@vinai.io>
2024-10-12 02:21:58 +0000

    fix

--- script/train_sft_fb_sp.sh
+++ script/train_sft_fb_sp.sh
@@ -1,41 +1,41 @@
-CUDA_VISIBLE_DEVICES=0 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --master_addr 127.0.0.1 --master_port 23756\
-                   src/train.py --output_dir ./save/sft/fb15k237/keallm/lora \
-                    --stage sft \
-                    --hop 1-hop \
-                    --model_name_or_path meta-llama/Llama-2-7b-chat-hf \
-                    --language_model_path meta-llama/Llama-2-7b-chat-hf \
-                    --kge_model_path ledong0110/FB15k-237-KGE-Roberta-Base\
-                    --model_type lorra \
-                    --template llama2_keallm \
-                    --num_query_tokens 8 \
-                    --train_from_scratch true \
-                    --num_train_epochs 3 \
-                    --save_total_limit 3 \
-                    --load_best_model_at_end true\
-                    --eval_strategy steps \
-                    --save_strategy steps \
-                    --save_steps 5000 \
-                    --eval_steps 5000 \
-                    --logging_first_step true \
-                    --logging_steps 20 \
-                    --bf16 true \
-                    --do_train true \
-                    --do_eval true\
-                    --predict_with_generate false \
-                    --do_predict false \
-                    --top_k 1 \
-                    --max_new_tokens 32 \
-                    --learning_rate 1.0e-4 \
-                    --warmup_ratio 0.1 \
-                    --lr_scheduler_type cosine \
-                    --eval_dataset FB15k-237_roberta \
-                    --ignore_pad_token_for_loss true \
-                    --per_device_eval_batch_size 8 \
-                    --per_device_train_batch_size 4 \
-                    --gradient_accumulation_steps 2\
-                    --dataset FB15k-237_roberta \
-                    --tokenized_path ./tokenized_data/FB15k-237 \
-                    --deepspeed ./ds2.json
+# CUDA_VISIBLE_DEVICES=0 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --master_addr 127.0.0.1 --master_port 23756\
+#                    src/train.py --output_dir ./save/sft/fb15k237/keallm/lora \
+#                     --stage sft \
+#                     --hop 1-hop \
+#                     --model_name_or_path meta-llama/Llama-2-7b-chat-hf \
+#                     --language_model_path meta-llama/Llama-2-7b-chat-hf \
+#                     --kge_model_path ledong0110/FB15k-237-KGE-Roberta-Base\
+#                     --model_type lorra \
+#                     --template llama2_keallm \
+#                     --num_query_tokens 8 \
+#                     --train_from_scratch true \
+#                     --num_train_epochs 3 \
+#                     --save_total_limit 3 \
+#                     --load_best_model_at_end true\
+#                     --eval_strategy steps \
+#                     --save_strategy steps \
+#                     --save_steps 5000 \
+#                     --eval_steps 5000 \
+#                     --logging_first_step true \
+#                     --logging_steps 20 \
+#                     --bf16 true \
+#                     --do_train true \
+#                     --do_eval true\
+#                     --predict_with_generate false \
+#                     --do_predict false \
+#                     --top_k 1 \
+#                     --max_new_tokens 32 \
+#                     --learning_rate 1.0e-4 \
+#                     --warmup_ratio 0.1 \
+#                     --lr_scheduler_type cosine \
+#                     --eval_dataset FB15k-237_roberta \
+#                     --ignore_pad_token_for_loss true \
+#                     --per_device_eval_batch_size 8 \
+#                     --per_device_train_batch_size 4 \
+#                     --gradient_accumulation_steps 2\
+#                     --dataset FB15k-237_roberta \
+#                     --tokenized_path ./tokenized_data/FB15k-237 \
+#                     --deepspeed ./ds2.json
                     # --resume_from_checkpoint true\
 
 CUDA_VISIBLE_DEVICES=0 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --master_addr 127.0.0.1 --master_port 23756\
@@ -59,7 +59,7 @@ CUDA_VISIBLE_DEVICES=0 torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1  --m
                     --logging_first_step true \
                     --logging_steps 20 \
                     --bf16 true \
-                    --do_train false \
+                    --do_train true \
                     --do_eval false\
                     --predict_with_generate true \
                     --do_predict true \
